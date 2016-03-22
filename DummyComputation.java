6632318d30e419baf7fa691379fb805895e1c5f5
Cheryl Shang <z4shang@uwaterloo.ca>
2016-03-21 23:48:57 -0400

    add jar file

---
+++ DummyComputation.java
@@ -0,0 +1,25 @@
+package mypackage;
+
+import org.apache.giraph.graph.BasicComputation;
+import org.apache.giraph.graph.Vertex;
+import org.apache.hadoop.io.Writable;
+import org.apache.hadoop.io.WritableComparable;
+
+import org.apache.giraph.conf.LongConfOption;
+import org.apache.giraph.edge.Edge;
+import org.apache.hadoop.io.DoubleWritable;
+import org.apache.hadoop.io.FloatWritable;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.log4j.Logger;
+
+import java.io.IOException;
+
+public class DummyComputation extends BasicComputation<
+        LongWritable, DoubleWritable, FloatWritable, DoubleWritable> {
+    @Override
+    public void compute(Vertex<LongWritable, DoubleWritable, FloatWritable> vertex,
+                        Iterable<DoubleWritable> messages) throws IOException {
+        vertex.setValue(new DoubleWritable(1.0));
+        vertex.voteToHalt();
+    }
+}
